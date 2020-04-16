cd495a00c994f31173b775062601fd40792649da
hishamss <hishamss90@gmail.com>
2020-04-16 12:33:38 -0700

    added user validation to accept only integers

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 require("console.table");
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,13 +11,44 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  afterconnect();
+  displayall();
 });
 
-function afterconnect() {
+function displayall() {
   connection.query("select * from products", function (err, res) {
     if (err) throw err;
     console.table(res);
-    connection.end();
+    buyitem();
   });
+}
+
+function buyitem() {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        message: "please type the item id you would like to buy",
+        validate: function (value) {
+          // accept only integer numbers
+          if (Number.isInteger(Number(value))) {
+            return true;
+          }
+          return false;
+        },
+      },
+      {
+        name: "quantity",
+        message: "Please enter the quantity?",
+        validate: function (value) {
+          // accept only integer numbers
+          if (Number.isInteger(Number(value))) {
+            return true;
+          }
+          return false;
+        },
+      },
+    ])
+    .then(function (result) {
+      console.log(result.item, result.quantity);
+    });
 }
